10fe40c71a0cf5fddc61a24751dee02c5554a4ec
niristotle okram <nirish.okram@gmail.com>
2016-05-05 17:26:02 -0500

    some more change to the deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,11 +1,11 @@
 
 
-$ProjectPath=$args[0]
-echo "Downloading artifact to $ProjectPath"
+$Project=$args[0]
+echo "Downloading artifact to \\WIN-VQR8SOC8MKP\App\$Project"
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile("http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/%BUILD_NUMBER/webcontent.zip", "$ProjectPath");
-echo "Extracting the artifact $ProjectPath"
-7z x $ProjectPath -y
+$wc.DownloadFile("http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/%BUILD_NUMBER/$ProjectPath.zip", "\\WIN-VQR8SOC8MKP\App\$ProjectPath");
+echo "Extracting the artifact \\WIN-VQR8SOC8MKP\App\$Project"
+7z x \\WIN-VQR8SOC8MKP\App\$Project -y
 echo "Deleting the zip file"
-rmdir -Force $ProjectPath
-echo "Deleted the zip file $ProjectPath"
+rmdir -Force \\WIN-VQR8SOC8MKP\App\$Project
+echo "Deleted the zip file \\WIN-VQR8SOC8MKP\App\$Project.zip"
