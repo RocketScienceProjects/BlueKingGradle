0da6284ccd67648cc049d2369cc12d3cc98c371e
niristotle okram <nirish.okram@gmail.com>
2016-05-05 17:19:02 -0500

    update the nexus repo url construct

--- deploy.ps1
+++ deploy.ps1
@@ -3,7 +3,7 @@
 $ProjectPath=$args[0]
 echo "Downloading artifact to $ProjectPath"
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile("http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/source/1.0/bidservice.zip", "$ProjectPath");
+$wc.DownloadFile("http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/%BUILD_NUMBER/webcontent.zip", "$ProjectPath");
 echo "Extracting the artifact $ProjectPath"
 7z x $ProjectPath -y
 echo "Deleting the zip file"
