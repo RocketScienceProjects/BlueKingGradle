d558d34383a1505dbeff8eaa65893765663f9bcd
niristotle okram <nirish.okram@gmail.com>
2016-05-05 17:56:08 -0500

    corrects a typo

--- deploy.ps1
+++ deploy.ps1
@@ -3,7 +3,7 @@
 $Project=$args[0]
 echo "Downloading artifact to \\WIN-VQR8SOC8MKP\App\$Project"
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile("http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/%BUILD_NUMBER/$ProjectPath.zip", "\\WIN-VQR8SOC8MKP\App\$ProjectPath");
+$wc.DownloadFile("http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/%BUILD_NUMBER%/$ProjectPath.zip", "\\WIN-VQR8SOC8MKP\App\$ProjectPath");
 echo "Extracting the artifact \\WIN-VQR8SOC8MKP\App\$Project"
 7z x \\WIN-VQR8SOC8MKP\App\$Project -y
 echo "Deleting the zip file"
