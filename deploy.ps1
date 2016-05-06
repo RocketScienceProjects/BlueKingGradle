d58e4dd37e189b0f2b81205b55acf0beef4cbabf
niristotle okram <nirish.okram@gmail.com>
2016-05-06 11:54:03 -0500

    fixes the incorrect path issue

--- deploy.ps1
+++ deploy.ps1
@@ -3,7 +3,7 @@
 $Project=$args[0]
 echo "Downloading artifact to \\WIN-VQR8SOC8MKP\App\$Project"
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile("http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/%BUILD_NUMBER%/$ProjectPath.zip", "\\WIN-VQR8SOC8MKP\App\$ProjectPath.zip");
+$wc.DownloadFile("http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/%BUILD_NUMBER%/$Project.zip", "\\WIN-VQR8SOC8MKP\App\$Project.zip");
 echo "Extracting the artifact \\WIN-VQR8SOC8MKP\App\$Project"
 7z x \\WIN-VQR8SOC8MKP\App\$Project.zip -y
 echo "Deleting the zip file"
