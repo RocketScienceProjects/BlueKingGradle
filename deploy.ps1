a4e5ee380138acbaefd51ed3c73b32711110cc00
niristotle okram <nirish.okram@gmail.com>
2016-05-06 12:18:25 -0500

    testing with local drive

--- deploy.ps1
+++ deploy.ps1
@@ -3,9 +3,9 @@ $Project=$args[0]
 $Build_Number=$args[1]
 echo "Downloading artifact to \\WIN-VQR8SOC8MKP\App\$Project from http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/$Build_Number/$Project.zip"
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile("http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/%BUILD_NUMBER%/$Project.zip", "\\WIN-VQR8SOC8MKP\App\$Project.zip");
-echo "Extracting the artifact \\WIN-VQR8SOC8MKP\App\$Project"
-7z x \\WIN-VQR8SOC8MKP\App\$Project.zip -y
+$wc.DownloadFile("http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/%BUILD_NUMBER%/$Project.zip", "c:\$Project.zip");
+echo "Extracting the artifact c:\$Project.zip"
+7z x c:\$Project.zip -y
 echo "Deleting the zip file"
-rmdir -Force \\WIN-VQR8SOC8MKP\App\$Project.zip
-echo "Deleted the zip file \\WIN-VQR8SOC8MKP\App\$Project.zip"
+rmdir -Force c:\$Project.zip
+echo "Deleted the zip file c:\$Project.zip"
