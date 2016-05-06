aab1af301c923accd0c1437be201eb66784284aa
niristotle okram <nirish.okram@gmail.com>
2016-05-06 11:49:46 -0500

    fixes the syntax error for deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -3,9 +3,9 @@
 $Project=$args[0]
 echo "Downloading artifact to \\WIN-VQR8SOC8MKP\App\$Project"
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile("http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/%BUILD_NUMBER%/$ProjectPath.zip", "\\WIN-VQR8SOC8MKP\App\$ProjectPath");
+$wc.DownloadFile("http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/%BUILD_NUMBER%/$ProjectPath.zip", "\\WIN-VQR8SOC8MKP\App\$ProjectPath.zip");
 echo "Extracting the artifact \\WIN-VQR8SOC8MKP\App\$Project"
-7z x \\WIN-VQR8SOC8MKP\App\$Project -y
+7z x \\WIN-VQR8SOC8MKP\App\$Project.zip -y
 echo "Deleting the zip file"
-rmdir -Force \\WIN-VQR8SOC8MKP\App\$Project
+rmdir -Force \\WIN-VQR8SOC8MKP\App\$Project.zip
 echo "Deleted the zip file \\WIN-VQR8SOC8MKP\App\$Project.zip"
