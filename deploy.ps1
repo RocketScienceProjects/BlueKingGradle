cfdcbb6d3e989f05584f544e0a85fa5d514583ae
niristotle okram <nirish.okram@gmail.com>
2016-05-06 12:01:33 -0500

    fucking tired of shit messages

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,7 @@
 
 
 $Project=$args[0]
-echo "Downloading artifact to \\WIN-VQR8SOC8MKP\App\$Project"
+echo "Downloading artifact to \\WIN-VQR8SOC8MKP\App\$Project from http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/%BUILD_NUMBER%/$Project.zip"
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile("http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/%BUILD_NUMBER%/$Project.zip", "\\WIN-VQR8SOC8MKP\App\$Project.zip");
 echo "Extracting the artifact \\WIN-VQR8SOC8MKP\App\$Project"
