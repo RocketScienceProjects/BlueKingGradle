3f59b7f4be1e4f14355e9ee162c58652fd500ba6
niristotle okram <nirish.okram@gmail.com>
2016-05-06 18:01:02 -0500

    testing this shit on a friday evening

--- deploy.ps1
+++ deploy.ps1
@@ -1,11 +1,12 @@
 
 $Project=$args[0]
 $Build_Number=$args[1]
-echo "Downloading artifact to \\WIN-VQR8SOC8MKP\App\$Project from http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/$Build_Number/$Project.zip"
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile("http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar/%BUILD_NUMBER%/$Project.zip", "c:\$Project.zip");
-echo "Extracting the artifact c:\$Project.zip"
-7z x c:\$Project.zip -y
-echo "Deleting the zip file"
-rmdir -Force c:\$Project.zip
-echo "Deleted the zip file c:\$Project.zip"
+echo "Downloading artifact"
+
+$Webclient = New-Object System.Net.WebClient
+$Nexusurl  = "http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar" + "/" + $ENV:BUILD_NUMBER + "/" + $ENV:Project
+echo $Nexusurl
+$Webclient.DownloadFile($Nexusurl, 'c:\webcontent.zip')
+
+
+
