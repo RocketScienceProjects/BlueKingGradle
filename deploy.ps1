c2768738983e15178fbd45af6b513f0258706f04
niristotle okram <nirish.okram@gmail.com>
2016-05-06 19:21:02 -0500

    Downloads to IIS root

--- deploy.ps1
+++ deploy.ps1
@@ -5,7 +5,7 @@ echo "Downloading artifact"
 $Webclient = New-Object System.Net.WebClient
 $Nexusurl  = "http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar" + "/" + $ENV:BUILD_NUMBER + "/" + $Project
 echo $Nexusurl
-$Webclient.DownloadFile($Nexusurl, 'c:\webcontent.zip')
+$Webclient.DownloadFile($Nexusurl, '\\WIN-RATABIECTJ2\App\webcontent.zip')
 
 
 
