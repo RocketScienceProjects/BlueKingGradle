4764e89dc2c48af73d316d3a28e2be6a5e5a7388
okram999 <nirish.okram@gmail.com>
2016-12-20 14:19:17 -0600

    Updates the POM.xml

---
+++ bin/WebContent/index.jsp
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Welcome to Rocket Science Projects</title>
+</head>
+<body>
+	<br>
+	<div style="text-align:center">
+		<h2>
+			Site After The Change....<br> <br>
+		</h2>
+		<h3>
+			<a href="welcome.html">Click here to See Welcome Message... </a>
+		</h3>
+	</div>
+</body>
+</html>
---
+++ bin/deploy.ps1
@@ -0,0 +1,9 @@
+#this is a test line, to induce an scm change another one......
+$Project=$args[0]
+$Build_Number=$args[1]
+echo "Downloading artifact"
+
+$Webclient = New-Object System.Net.WebClient
+$Nexusurl  = "http://ec2-54-213-106-231.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/org/bar" + "/" + $ENV:BUILD_NUMBER + "/" + $Project
+echo $Nexusurl
+$Webclient.DownloadFile($Nexusurl, '\\WIN-RATABIECTJ2\App\webcontent.zip')
